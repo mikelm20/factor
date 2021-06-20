f9e7d6b906788e1e67053922ddb042ce802dc0d9
Mikel Martin <martinmikel20@gmail.com>
2021-06-20 18:56:25 -0500

    page not found wildcard

--- factor-front-end/factor-front-end/src/app/app-routing.module.ts
+++ factor-front-end/factor-front-end/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { UploadComponent } from './upload/upload.component';
 import { AppComponent } from './app.component';
 import { VideoComponent } from './video/video.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', component: AppComponent},
   { path: 'upload', component: UploadComponent},
-  { path: 'video', component: VideoComponent}
+  { path: 'video', component: VideoComponent},
+  { path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
--- factor-front-end/factor-front-end/src/app/app.module.ts
+++ factor-front-end/factor-front-end/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { UploadComponent } from './upload/upload.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { VideoComponent } from './video/video.component';
 import { MatIconModule } from '@angular/material/icon'; 
- import {MatProgressBarModule} from '@angular/material/progress-bar'; 
+ import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'; 
 
 @NgModule({
   declarations: [
     AppComponent,
     UploadComponent,
-    VideoComponent
+    VideoComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
