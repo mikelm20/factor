92a188825386de4eec409774cf0437b60c89aa06
Mikel Martin <martinmikel20@gmail.com>
2021-06-22 18:00:27 -0500

    Cors setup

--- factor-back-end/factor-back-end/app.js
+++ factor-back-end/factor-back-end/app.js
@@ -5,10 +5,27 @@ const path = require('path');
 const routes = require('./routes/index');
 const app = express();
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect (Factor front-end)
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 // Route /api to Routes directory
 app.use('/', routes);
-
-app.use(cors())
 
 app.listen(5000, () => {
     console.log('Listening on port 5000!')
